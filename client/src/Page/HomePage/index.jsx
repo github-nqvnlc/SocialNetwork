fa49d36effe032e13e1d99bddc2536b0e2342e3f
github.nqvnlc <locnvgcd18350@fpt.edu.vn>
2023-05-24 00:23:37 +0700

    first commit

---
+++ client/src/Page/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Navbar from "Page/Navbar";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
